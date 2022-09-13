69dabcaf45e92e127555ed29d697cc15e93247c1
Rishi Sain <rishi.sain@gramener.org>
2022-09-13 11:30:51 +0530

    Background color change by color-picker - Resolved

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import SketchExample from './SketchExample';
+
+function App() {
+  return (
+    <div className="App">
+      <SketchExample />
+    </div>
+  );
+}
+
+export default App;
